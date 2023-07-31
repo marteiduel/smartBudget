b75a51db28f0a1d26415f568bc983a0765953058
Duel <51247353+marteiduel@users.noreply.github.com>
2023-07-30 21:47:53 -0600

    start categories page

---
+++ src/app/categories/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Categories | Smart Budget",
+  description: "Organize your budget with categories, and subcategories",
+};
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <div>{children}</div>
+    </div>
+  );
+}
--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -1,5 +1,31 @@
-function Categories() {
-  return <div>Categories</div>;
+import Link from "next/link";
+import styles from "./styles.module.css";
+import NextTopLoader from "nextjs-toploader";
+async function getData() {
+  const res = await fetch(
+    "https://marteiduel.com/smartbudget/get_categories.php"
+  );
+
+  return res.json();
 }
 
-export default Categories;
+export default async function Page() {
+  const data = await getData();
+  console.log(data);
+
+  return (
+    <div>
+      <header className={styles.header}>
+        <Link className={styles.back} href="/">
+          Back
+        </Link>
+        <h1 className={styles.pageTitle}>Categories</h1>
+      </header>
+      <div>
+        {data.map((category) => {
+          return <div key={category.categoryId}>{category.category_name}</div>;
+        })}
+      </div>
+    </div>
+  );
+}

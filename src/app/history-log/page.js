228dc853a1bf1bcd7c33cf55eaeaf9e03350ee1e
Duel <51247353+marteiduel@users.noreply.github.com>
2023-07-31 11:55:58 -0600

    history log page
    need to refactor styles to global because they are repeating too much

--- src/app/history-log/page.js
+++ src/app/history-log/page.js
@@ -1,7 +1,40 @@
-import React from "react";
+import Link from "next/link";
+import styles from "./styles.module.css";
+import Image from "next/image";
+// import NextTopLoader from "nextjs-toploader";
+async function getHistoryLog() {
+  const res = await fetch(
+    "https://marteiduel.com/smartbudget/get_history_log.php"
+  );
 
-function HistoryLog() {
-  return <div>HistoryLog</div>;
+  return res.json();
 }
 
-export default HistoryLog;
+export default async function HistoryLog() {
+  const data = await getHistoryLog();
+  console.log(data);
+
+  return (
+    <div>
+      <header className={styles.header}>
+        <Link className={styles.back} href="/">
+          Back
+        </Link>
+        <h1 className={styles.pageTitle}>History Log</h1>
+      </header>
+
+      <div className={styles.categoriesList}>
+        {data.map((transaction) => {
+          return (
+            <div key={transaction.id} className={styles.categoryName}>
+              <div className={styles.categoryAndIcons}>
+                <div>{transaction.description}</div>
+                <div className={styles.icons}>${transaction.amount}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}

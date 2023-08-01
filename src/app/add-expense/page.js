7f0057c9c8cd26b0938ef730414bc3b063a7c6f7
Duel <51247353+marteiduel@users.noreply.github.com>
2023-08-01 11:42:43 -0600

    send payload (add expense page)
    corrections on css and sending payload to api for new expense

--- src/app/add-expense/page.js
+++ src/app/add-expense/page.js
@@ -7,6 +7,8 @@ export default function HistoryLog() {
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("");
   const [today, setToday] = useState("");
+  const [amount, setAmount] = useState("");
+  const [description, setDescription] = useState("");
   // const [subcategories, setSubCategories] = useState([]);
   // const [selectedSubCategory, setSelectedSubCategory] = useState("");
 
@@ -35,6 +37,25 @@ export default function HistoryLog() {
     //   .catch((error) => console.error("Error fetching subcategories:", error));
   }
 
+  function addExpense(e) {
+    e.preventDefault();
+    fetch("https://marteiduel.com/smartbudget/add_expense.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        categoryId: selectedCategory,
+        amount: amount,
+        date: today,
+        description: description,
+        userId: 1,
+      }),
+    });
+    setAmount("");
+    setDescription("");
+  }
+
   function todaysDate() {
     const date = new Date();
     const year = date.getFullYear();
@@ -60,7 +81,7 @@ export default function HistoryLog() {
         <form
           className={styles.form}
           onSubmit={(e) => {
-            e.preventDefault();
+            addExpense(e);
           }}
         >
           <div className={styles.labels}>Select Category</div>
@@ -72,7 +93,13 @@ export default function HistoryLog() {
               <option value="">Select a value</option>
               {categories.map((category) => {
                 return (
-                  <option key={category.categoryId} value={category.categoryId}>
+                  <option
+                    key={category.categoryId}
+                    value={category.categoryId}
+                    name="category"
+                    id="category"
+                    required
+                  >
                     {category.category_name}
                   </option>
                 );
@@ -81,41 +108,31 @@ export default function HistoryLog() {
           ) : (
             "Loading..."
           )}
-          {/* 
-          <div className={styles.labels}>Select Sub-Category</div>
-          {subcategories ? (
-            <select
-              className={styles.addCategory}
-              onChange={(e) => setSelectedSubCategory(e.target.value)}
-            >
-              <option value="">Select a value</option>
-              {subcategories.map((category) => {
-                return (
-                  <option key={category.id} value={category.id}>
-                    {category.subCategoryName}
-                  </option>
-                );
-              })}
-            </select>
-          ) : (
-            "Loading..."
-          )} */}
 
           <div className={styles.labels}>Amount</div>
           <input
             className={styles.addCategory}
             type="float"
             placeholder="Enter Amount"
+            required
+            onChange={(e) => setAmount(e.target.value)}
+            id="amount"
           />
 
           <div className={styles.labels}>Expense Date</div>
-          <input className={styles.addCategory} type="date" value={today} />
+          <input
+            className={styles.addCategory}
+            type="date"
+            onChange={(e) => setToday(e.target.value)}
+            value={today}
+          />
 
           <div className={styles.labels}>Expense Description</div>
           <textarea
             className={styles.description}
             type="textarea"
             placeholder="Enter Description"
+            onChange={(e) => setDescription(e.target.value)}
           />
 
           <input className={styles.addCategory} type="submit" value="Submit" />

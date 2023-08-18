136f006f34ad24c3353e3a8c09a3e5af3c2a5688
Duel <51247353+marteiduel@users.noreply.github.com>
2023-08-17 21:13:23 -0600

    pwa

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="manifest" href="/manifest.json" />
       <body className={inter.className}>{children}</body>
     </html>
   );

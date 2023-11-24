e19314655c5e8ea4284ea7a154ed0d935f639ede
Duel <marteiwgc@gmail.com>
2023-11-24 11:15:56 -0700

    transactions by category component

--- src/app/review-budget/[id]/page.js
+++ src/app/review-budget/[id]/page.js
@@ -1,13 +1,14 @@
 "use client"
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import Link from "next/link";
+import styles from "../styles.module.css";
 
 function SingleCategoryTransactions(urlId) {
     const id = urlId.params.id
     const [data, setData] = useState([]);
 
     useEffect(() => {
-      console.log(id);
       fetch(
         `https://marteiduel.com/smartbudget/transactions_by_category.php?category_id=${id}`
       )
@@ -18,7 +19,7 @@ function SingleCategoryTransactions(urlId) {
           return res.json();
         })
         .then((data) => {
-          console.log(data);
+          console.log(data)
           setData(data);
         })
         .catch((error) => {
@@ -33,18 +34,37 @@ function SingleCategoryTransactions(urlId) {
     const router = useRouter();
 
 
-  return <div> 
-    {data.map((transaction) => {
-      return (
-        <div key={transaction.id}>
-          <p>{transaction.id}</p>
-          <p>${transaction.amount}</p>
-          <p>{transaction.description}</p>
-          <p>{transaction.transaction_date}</p>
+  return (
+    <div>
+      <header className="header">
+        <Link className="back" href="/review-budget">
+          Back
+        </Link>
+        <h1 className="pageTitle">Review Budget</h1>
+      </header>
+
+      <div className={`${styles.backBox} backBox`}>
+        <div className="categoryItem">
+          <div className="spaceBetween">
+            <p className="pr-4">Date</p>
+            <p>Category</p>
+            <p className="pr-4">Amount</p>
+          </div>
         </div>
-      );
-    })}
-  </div>;
+        {data.map((transaction) => {
+          return (
+            <div key={transaction.id} className="categoryItem">
+              <div className="spaceBetween">
+                <div>{transaction.transaction_date}</div>
+                <div>{transaction.description}</div>
+                <div>${transaction.amount}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default SingleCategoryTransactions;

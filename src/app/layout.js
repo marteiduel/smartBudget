282c22a6406680bc9c4fe0ed01549e001ae1a0c6
Duel <51247353+marteiduel@users.noreply.github.com>
2023-12-22 20:20:41 -0700

    beginning of authentication

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
+"use client";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRoutes from "./util/PrivateRoutes";
+import Login from "./login/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +16,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <link rel="manifest" href="/manifest.json" />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Router>
+          <Routes>
+            <Route element={<PrivateRoutes />}>
+              <Route path="/" element={children} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+      </body>
     </html>
   );
 }

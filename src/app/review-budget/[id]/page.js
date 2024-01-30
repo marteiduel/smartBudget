8c732abc9fbdcfc9b6482f78f2a142d398c62c3c
Duel <51247353+marteiduel@users.noreply.github.com>
2024-01-29 20:40:07 -0700

    review budget by item new ui working

--- src/app/review-budget/[id]/page.js
+++ src/app/review-budget/[id]/page.js
@@ -1,54 +1,61 @@
 "use client"
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import styles from "../styles.module.css";
 import EditTransaction from "./Components/EditTransaction";
+import {View, Cell, Column, Row, TableView, TableBody, TableHeader} from '@adobe/react-spectrum'
 
 function SingleCategoryTransactions(urlId) {
-    const id = urlId.params.id
-    const [data, setData] = useState([]);
-    const [showModal, setShowModal] = useState(false);
-    const [selectedTransaction, setSelectedTransaction] = useState(null);
-
-    useEffect(() => {
-      fetch(
-        `https://marteiduel.com/smartbudget/transactions_by_category.php?category_id=${id}`
-      )
+  const id = urlId.params.id
+  const [data, setData] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedTransaction, setSelectedTransaction] = useState(null);
+  
+  useEffect(() => {
+    fetch(
+      `https://marteiduel.com/smartbudget/transactions_by_category.php?category_id=${id}`
+    )
 .then((res) => {
-          if (!res.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data)
-          setData(data);
-        })
-      .catch((error) => {
-          console.error(
-            "There has been a problem with your fetch operation:",
-            error
-          );
-        });
-      }, [id]);
-        
-      function openPopout(transaction) {
-        setSelectedTransaction(transaction);
-        setShowModal(true);
-      }
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data)
+        setData(data);
+      })
+    .catch((error) => {
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        );
+      });
+    }, [id]);
+      
+    function openPopup(transactionId) {
+      var transaction = data.find((x) => x.id == transactionId);
+      setSelectedTransaction(transaction);
+      setShowModal(true);
+    }
 
-  const closePopout = (e) => {
+  const closePopup = (e) => {
     e.preventDefault();
     setShowModal(false);
   };
 
 
   return (
-    <div>
+    <View 
+    backgroundColor="gray-50"
+    padding="size-100"
+    borderRadius="medium"
+    borderWidth="thin"
+    borderColor="blue-500"
+    >
       {showModal && (
         <EditTransaction
           transaction={selectedTransaction}
-          onClose={closePopout}
+          onClose={closePopup}
           categoryId={id}
         />
       )}
@@ -59,34 +66,27 @@ function SingleCategoryTransactions(urlId) {
         <h1 className="pageTitle">Review Budget</h1>
       </header>
 
-      <div className={`${styles.backBox} backBox`}>
-        <div className="categoryItem">
-          <div className="spaceBetween">
-            <p>Category</p>
-            <p className="pr-4">Amount</p>
-          </div>
-        </div>
-        {data.map((transaction) => {
-          return (
-            <div
-              key={transaction.id}
-              onClick={() => openPopout(transaction)}
-              className="categoryItem"
-            >
-              <div className="spaceBetween">
-                <div>
-                  {transaction.description}{" "}
-                  <p style={{ fontSize: "10px" }}>
-                    {transaction.transaction_date.split(" ")[0]}
-                  </p>
-                </div>
-                <div>${transaction.amount}</div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+      <TableView onAction={(item) => openPopup(item)}>
+        <TableHeader>
+          <Column showDivider>
+            Description
+          </Column>
+          <Column showDivider>
+            Amount
+          </Column>
+        </TableHeader>
+        <TableBody items={data} >
+          {(item) => (
+            <Row key={item.id} >
+              {item.description ? <Cell >{item.description}</Cell> : <Cell >No description</Cell>}
+              <Cell>
+                {item.amount}
+              </Cell>
+            </Row>
+          )}  
+        </TableBody>
+      </TableView>
+    </View>
   );
 }
 

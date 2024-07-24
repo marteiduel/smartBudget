c745cfe3a5a9e15a753c82070ddc197fe50715ef
Duel <51247353+marteiduel@users.noreply.github.com>
2024-07-23 22:39:54 -0600

    information ready to sent to server

--- src/app/savings/options.js
+++ src/app/savings/options.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
-import { ActionButton, DialogContainer, Heading, Divider, Content, Form, TextField, ButtonGroup, Button, Dialog, MenuTrigger, Menu, Item, Text, ComboBox } from '@adobe/react-spectrum';
+import { ActionButton, DialogContainer, Heading, Divider, Content, Form, TextField, ButtonGroup, Button, Dialog, MenuTrigger, Menu, Item, Text, ComboBox, NumberField } from '@adobe/react-spectrum';
 import More from '@spectrum-icons/workflow/More';
 import { useDialogContainer } from '@adobe/react-spectrum';
 
 export function Options({ category, categories }) {
   const [dialog, setDialog] = useState(null);
 
-  const categoryName = category.category_name;
-  const categoryAmount = category.savings;
-  console.log(categories, 'categories in OptionsPASO1');
+  const selectedCategoryName = category.category_name;
+  const selectedCategoryAmount = category.savings;
+  const selectedCategoryId = category.categoryId;
+  console.log(category, "category");
 
   return (
     <>
@@ -22,16 +23,16 @@ export function Options({ category, categories }) {
         </Menu>
       </MenuTrigger>
       <DialogContainer onDismiss={() => setDialog(null)}>
-        {dialog === 'edit' && <EditDialog categoryName={categoryName} categoryAmount={categoryAmount} />}
+        {dialog === 'edit' && <EditDialog selectedCategoryName={selectedCategoryName} categoryAmount={categoryAmount} />}
         {dialog === 'transfer' && (
-          <TransferDialog categoryName={categoryName} categoryAmount={categoryAmount} categories={categories} />
+          <TransferDialog selectedCategoryName={selectedCategoryName} selectedCategoryAmount={selectedCategoryAmount} selectedCategoryId={selectedCategoryId} categories={categories} />
         )}
       </DialogContainer>
     </>
   );
 }
 
-function EditDialog({ categoryName, categoryAmount }) {
+function EditDialog({ selectedCategoryName, selectedCategoryAmount, }) {
   const dialog = useDialogContainer();
 
   return (
@@ -53,34 +54,49 @@ function EditDialog({ categoryName, categoryAmount }) {
   );
 }
 
-function TransferDialog({ categoryName, categoryAmount, categories }) {
+function TransferDialog({ selectedCategoryName, selectedCategoryAmount, selectedCategoryId, categories }) {
   const dialog = useDialogContainer();
-  const [categoryId, setCategoryId] = useState(null);
+  // console.log(categories, "categories"); 
+  const [destinationCategoryId, setDestinationCategoryId] = useState(null);
+  const [amountToTransfer, setAmountToTransfer] = useState(0);
+  const [description, setDescription] = useState("");
 
-  console.log(categories, 'categories in Transfer');
+  const destinationCategoryInformation = categories.find(category => category.categoryId === destinationCategoryId);
+
+  const actualMoneyOnReceivingCategory = destinationCategoryInformation ? destinationCategoryInformation.category_budget : 0;
+
+  const newAmountReceivingCategory = parseFloat(actualMoneyOnReceivingCategory) + parseFloat(amountToTransfer);
+
+  const ReminderAmount = parseFloat(selectedCategoryAmount) - parseFloat(amountToTransfer);
+
+  const sendInformation = console.log("Sending information to the server", {
+  newAmountReceivingCategory, description, destinationCategoryId, selectedCategoryId, ReminderAmount}
+  )
+
 
   return (
     <Dialog height="calc(size-6000 - size-700)">
-      <Heading>Transfer from: {categoryName}</Heading>
+      <Heading>Transfer from: {selectedCategoryName}</Heading>
       <Divider />
       <Content>
         <Form labelPosition="side" width="100%">
-          <Text>Current Amount: {categoryAmount}</Text>
+          <Text>Current Amount: {selectedCategoryAmount}</Text>
           <ComboBox
             label="Category Destination"
             defaultItems={categories}
-            selectedKey={categoryId}
-            onSelectionChange={selected => setCategoryId(selected)}
+            selectedKey={destinationCategoryId}
+            onSelectionChange={selected => setDestinationCategoryId(selected)}
+            isRequired
           >
             {item => <Item key={item.categoryId}>{item.category_name}</Item>}
           </ComboBox>
-          <TextField autoFocus label="Amount" />
-          <TextField autoFocus label="Description" />
+          <NumberField label="Amount" value={amountToTransfer} minValue={0} maxValue={selectedCategoryAmount} onChange={setAmountToTransfer} isRequired/>
+          <TextField autoFocus label="Description" value={description} onChange={setDescription} isRequired/>
         </Form>
       </Content>
       <ButtonGroup>
         <Button variant="secondary" onPress={dialog.dismiss}>Cancel</Button>
-        <Button variant="accent" onPress={dialog.dismiss}>Save</Button>
+        <Button type='submit' variant="accent" onPress={dialog.dismiss}>Save</Button>
       </ButtonGroup>
     </Dialog>
   );
--- src/app/savings/page.js
+++ src/app/savings/page.js
@@ -32,8 +32,6 @@ export default function ReviewBudget() {
     getData();
   }, []);
 
-  console.log(categories, 'categoriesOutside');
-
   return (
     <View 
       backgroundColor="gray-50"
